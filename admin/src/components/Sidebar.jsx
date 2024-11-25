413fd55ad7c3326a22fc8ee3bb4b75e65b98b26c
Aryan Thakur <aryanthakur30082004@gmail.com~>
2024-11-25 20:57:23 +0530

    .

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,5 +1,4 @@
-console.log('Admin Token:', aToken)
-console.log('Doctor Token:', dToken)
+
 import React, { useContext } from 'react'
 import { assets } from '../assets/assets'
 import { NavLink } from 'react-router-dom'
