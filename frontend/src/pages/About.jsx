33a7d585b7fbea509ea75683a66ab34e597177e3
Aryan123656 <guptaaryan123656@gmail.com>
2024-11-26 20:38:26 +0530

    .

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
         <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
         <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
           <p>Welcome to VitalWeb, your comprehensive solution for hospital management. We are committed to revolutionizing healthcare administration by providing cutting-edge tools that streamline processes, enhance efficiency, and ensure exceptional patient care.</p>
-          <p>VitalWeb is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, Prescripto is here to support you every step of the way.</p>
+          <p>VitalWeb is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, VitalWeb is here to support you every step of the way.</p>
           <b className='text-gray-800'>Our Vision</b>
           <p>Our vision at VitalWeb is to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you to access the care you need, when you need it.</p>
         </div>
