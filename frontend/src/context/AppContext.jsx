52f144da9f6ebeff29ef4bbd6c3c11d7863acef3
Aryan Thakur <aryanthakur30082004@gmail.com~>
2024-12-05 16:31:16 +0530

    .

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -15,7 +15,7 @@ const AppContextProvider = (props) => {
     const getDoctosData = async () => {
 
         try {
-
+  
             const { data } = await axios.get(backendUrl + '/api/doctor/list');
             if (data.success) {
                 setDoctors(data.doctors);
--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -130,11 +130,16 @@ const Appointment = () => {
             <div className='sm:ml-72 sm:pl-4 mt-8 font-medium text-[#565656]'>
                 <p >Booking slots</p>
                 <div className='flex gap-3 items-center w-full overflow-x-scroll mt-4'>
-                    {docSlots.length && docSlots.map((item, index) => (
+                    
+                    {docSlots.length && docSlots.map((item, index =0) => (
+                        <>
+                        {
+                            !item[0]?<></>:
                         <div onClick={() => setSlotIndex(index)} key={index} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? 'bg-primary text-white' : 'border border-[#DDDDDD]'}`}>
-                            <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
+                           <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
                             <p>{item[0] && item[0].datetime.getDate()}</p>
                         </div>
+}</>
                     ))}
                 </div>
                 <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
